5a190bf2ff19db807197ccfdff0bacbd1b42db0e
sangkips <sangkipkoech@gmail.com>
2022-10-27 14:35:23 +0300

    added cors-headers

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,12 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'https://34.125.33.64:8000',
+    'http://127.0.0.1:8000',
+   
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
