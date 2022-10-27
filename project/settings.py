2413637e241eb49bddaea54a9610801dffdcbb2a
sangkips <sangkipkoech@gmail.com>
2022-10-27 15:39:31 +0300

    added procfile+runtime.txt

--- project/settings.py
+++ project/settings.py
@@ -1,16 +1,17 @@
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jn5(n11ip@ng5&82x*qp9b_h8i$33c$*s)zv=am7)^0f4a5j@h'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
