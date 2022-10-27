468b752c8cbe124df3c2dd9948b7636dec5ea6ca
sangkips <sangkipkoech@gmail.com>
2022-10-27 17:39:07 +0300

    changed debug mode

--- project/settings.py
+++ project/settings.py
@@ -14,9 +14,9 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
